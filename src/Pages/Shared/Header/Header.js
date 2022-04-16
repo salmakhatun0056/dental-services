d60a3f39726f0ec2f483086b139105c4ccc0b047
salmakhatun12345e <ummesalma0056@gmail.com>
2022-04-16 22:26:13 +0600

    navbar end

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <CustomLink to="/features">Features</CustomLink>
+                            <CustomLink to="/pricing">Pricing</CustomLink>
+                        </Nav>
+                        <Nav>
+                            <CustomLink as={Link} to="/deets">More deets</CustomLink>
+                            <CustomLink as={Link} to="/memes">
+                                Dank memes
+                            </CustomLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
